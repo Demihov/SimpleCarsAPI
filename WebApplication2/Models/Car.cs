2f875d9f64aab73e17d6ae59975823c4b6570985
Oleh Demikhov <oleh_demikhov@epam.com>
2022-04-30 16:56:07 +0300

    initial commit

---
+++ WebApplication2/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication2.Models
+{
+	public class Car
+	{
+		public int Id { get; set; }
+		public string Brand { get; set; }
+		public string Model { get; set; }
+		public string Color { get; set; }
+		public int Year { get; set; }
+		public string BodyType { get; set; }
+		public string Fuel { get; set; }
+	}
+}
---
+++ WebApplication2/Models/CarsContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models
+{
+	public class CarsContext : DbContext
+	{
+		public DbSet<Car> Cars { get; set; }
+		public CarsContext(DbContextOptions<CarsContext> options)
+		    : base(options)
+		{
+			Database.EnsureCreated();
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Car>().HasKey(c => c.Id);
+
+			modelBuilder.Entity<Car>().HasData(
+				new Car
+				{
+					Id = 1,
+					Brand = "Skoda",
+					Model = "Fabia",
+					Color = "Grey",
+					BodyType = "Universal",
+					Fuel = "gasoline",
+					Year = 2002
+				},
+				new Car
+				{
+					Id = 2,
+					Brand = "Audi",
+					Model = "A4",
+					Color = "Black",
+					BodyType = "Universal",
+					Fuel = "diesel",
+					Year = 2004
+				},
+				new Car
+				{
+					Id = 3,
+					Brand = "Nissan",
+					Model = "Qashqai",
+					Color = "White",
+					BodyType = "Universal",
+					Fuel = "gasoline",
+					Year = 2021
+				});
+		}
+	}
+}
